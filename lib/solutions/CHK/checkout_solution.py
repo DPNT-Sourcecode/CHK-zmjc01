2025344958c1d38891fffd05ae58a1636adf84f2
sbx_user1051 <sbx_user1051@169.254.45.111>
2025-10-15 09:53:07 +0000

    Wed Oct 15 09:53:07 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,27 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        # 3 A cost 150 however we have special offer 130, which it will reduce the total count 20 every 3A products
+        # 2 B COST 30  however we have special offer for 45, which it will reduce this 15 every 2B products
+
+        prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+        discount = {'A': (3, 20), 'B': (2, 15)}
+        item_count = {}
+        for item in skus:
+            if item not in prices:
+                return -1
+            if item in item_count:
+                item_count[item] += 1
+            else:
+                item_count[item] = 1 
+        total = 0
+        for item, count in item_count.items():
+            total += count * prices[item]
+            if item in discount:
+                discount_count, discount_amount = discount[item]
+                total -= (count // discount_count) * discount_amount                
+        
+        return total
+
+        
+
--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -2,5 +2,9 @@ from solutions.CHK.checkout_solution import CheckoutSolution
 
 
 class TestCheckout():
+    def test_checkout_wrong_item(self):
+        assert CheckoutSolution().checkout('AZ') == -1
+
     def test_checkout(self):
-        assert CheckoutSolution().compute(1, 2) == 3
+        assert CheckoutSolution().checkout('') == 0
+
