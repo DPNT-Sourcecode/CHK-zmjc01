40713d20b0afd8d3072af93b5ecff7b55a985037
sbx_user1051 <sbx_user1051@169.254.45.111>
2025-10-15 10:26:52 +0000

    Wed Oct 15 10:26:52 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,7 +7,8 @@ class CheckoutSolution:
         # 2 B COST 30  however we have special offer for 45, which it will reduce this 15 every 2B products
 
         prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
-        discount = {'A': [(5, 50), (3, 20)], 'B': [(2, 15)], 'E': [(3, 80)]}
+        discount = {'A': [(5, 50), (3, 20)], 'B': [(2, 15)]}
+        free_item_offer = {'E': ('B', 2)} # for every 2 E you get one B free
         item_count = {}
         for item in skus:
             if item not in prices:
@@ -18,6 +19,10 @@ class CheckoutSolution:
                 item_count[item] = 1 
         total = 0
         for item, count in item_count.items():
+            if item in free_item_offer:
+                free_item, required_count = free_item_offer[item]
+                if free_item in item_count:
+                    item_count[free_item] = max(0, item_count[free_item] - (count // required_count))
             total += count * prices[item]
             if item in discount:
                 special_offers = discount[item]
@@ -30,3 +35,4 @@ class CheckoutSolution:
         return total
 
         
+
